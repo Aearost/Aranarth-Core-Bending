06d8d91ff4b02f7b62b1755a90a4775ae2a9c7d1
LeeYumHarbec <liam.harbec13@gmail.com>
2020-10-28 01:39:29 -0400

    Blindly added checks to see if the rank they click on is to rank themselves up. This will be tested tomorrow.

--- src/com/aearost/aranarthcore/event/RanksClick.java
+++ src/com/aearost/aranarthcore/event/RanksClick.java
@@ -23,84 +23,69 @@ public class RanksClick implements Listener {
 	 */
 	@EventHandler
 	public void onRankClick(final InventoryClickEvent e) {
-		
-        if (ChatUtils.stripColor(e.getView().getTitle()).equals("Aranarth Ranks")) {
-        	e.setCancelled(true);
-        	
-        	Player player = (Player) e.getWhoClicked();
-        	double balance = AranarthPlayerUtils.getBalance(player);
-        	int rank = AranarthPlayerUtils.getRank(player);
-        	
-        	int slot = e.getSlot();
-        	String clickedName = e.getClickedInventory().getItem(slot).getItemMeta().getDisplayName();
-        	
-        	boolean isRankup = false;
-        	boolean isClickedRankSameAsCurrent = false;
-        	boolean isClickedRankLowerThanCurrent = false;
-        	
-        	
-        	
-//        	// Peasant
-//        	if (slot == 4) {
-//        		if (rank > 0) {
-//        			isClickedRankSameAsCurrent = true;
-//        		}
-//        		player.closeInventory();
-//        	}
-//        	// Esquire
-//        	else if (slot == 12) {
-//        		
-//        		player.closeInventory();
-//        		
-//        	}
-//        	// Knight
-//        	else if (slot == 14) {
-//        		
-//        		player.closeInventory();
-//        	}
-//        	// Baron
-//        	else if (slot == 20) {
-//        		
-//        		player.closeInventory();
-//        	}
-//        	// Count
-//        	else if (slot == 22) {
-//        		
-//        		player.closeInventory();
-//        	}
-//        	// Duke
-//        	else if (slot == 24) {
-//        		
-//        		player.closeInventory();
-//        	}
-//        	// Prince
-//        	else if (slot == 30) {
-//        		
-//        		player.closeInventory();
-//        	}
-//        	// King
-//        	else if (slot == 32) {
-//        		
-//        		player.closeInventory();
-//        	}
-//        	// Emperor
-//        	else if (slot == 40) {
-//        		
-//        		player.closeInventory();
-//        	}
-        	
-        	if (isClickedRankSameAsCurrent) {
-        		player.sendMessage(ChatUtils.translateToColor("&cYou are already a " + clickedName));
-        	} else if (isClickedRankLowerThanCurrent) {
-        		player.sendMessage(ChatUtils.translateToColor("&cYou cannot rank down!"));
-        		return;
-        	} else if (isRankup) {
-        		
-        	}
-        	
-        	player.sendMessage(ChatUtils.translateToColor("&6You have ranked up to " + clickedName));
-        }
-		
+
+		if (ChatUtils.stripColor(e.getView().getTitle()).equals("Aranarth Ranks")) {
+			e.setCancelled(true);
+
+			Player player = (Player) e.getWhoClicked();
+			double balance = AranarthPlayerUtils.getBalance(player);
+			int rank = AranarthPlayerUtils.getRank(player);
+
+			int slot = e.getSlot();
+			String clickedName = e.getClickedInventory().getItem(slot).getItemMeta().getDisplayName();
+
+			boolean isRankup = false;
+			boolean isClickedRankSameAsCurrent = false;
+			boolean isClickedRankLowerThanCurrent = false;
+			boolean isClickedRankHigherThanCurrent = false;
+
+			String[] ranks = new String[] { "&a&l&nPeasant", "&d&l&nEsquire", "&7&l&nKnight", "&5&l&nBaron",
+					"&8&l&nCount", "&6&l&nDuke", "&b&l&nPrince", "&9&l&nKing", "&4&l&nEmperor" };
+			int[] positions = new int[] { 4, 12, 14, 20, 22, 24, 30, 32, 40 };
+			int position = 0;
+
+			while (position < positions.length) {
+				if (slot == positions[position]) {
+					// If they clicked on the same one
+					if (rank == position) {
+						isClickedRankSameAsCurrent = true;
+						break;
+					}
+					// If they clicked on a previous rank
+					else if (rank > position) {
+						isClickedRankLowerThanCurrent = true;
+						break;
+					}
+					// If they clicked on a rank ahead of the rank above their own
+					else if (position < rank + 1) {
+						isClickedRankHigherThanCurrent = true;
+					}
+					// If they clicked on the rank above their own
+					else if (position == rank + 1) {
+						isRankup = true;
+						break;
+					}
+				}
+				position++;
+			}
+			
+			// Feedback Messages and Functionality
+			if (isClickedRankSameAsCurrent) {
+				player.sendMessage(ChatUtils.translateToColor("&cYou are already a " + clickedName + "&c!"));
+			} else if (isClickedRankLowerThanCurrent) {
+				player.sendMessage(
+						ChatUtils.translateToColor("&cYou cannot rank back down to a " + clickedName + "&c!"));
+				return;
+			} else if (isClickedRankHigherThanCurrent) {
+				player.sendMessage(ChatUtils.translateToColor("&cYou must rankup to " + ranks[rank + 1] + " &cfirst!"));
+			} else if (isRankup) {
+				// Make new Inventory holder
+				// "Are you sure you would like to rankup?"
+				// Single-line GUI with a lime and red wool named "&a&lRankup to + ranks[position] + for x money" and "&c&lCancel Rankup"
+			}
+
+		}
+
 	}
 
 }
